3f213bbf7f30b1a937782485c8d5509f93e611bf
Jackey_glb <qi2599_glb@126.com>
2019-08-25 23:53:51 +0800

    百度分析

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,7 @@
 
 <script>
 	export default {
-		watch:{
-			$route(to){
-				if(to.path !== '/home'){
-					gtag('config', 'UA-145668721-1', { 'page_title': to.name, 'page_path': to.path });
-				}
-			}
-		}
+	
 	}
 </script>
 
