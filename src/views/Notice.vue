8ef81c5cf833009de7721b091dcf55aa39a909e2
Jackey <jackey@JackeydeiMac.lan>
2021-07-11 13:31:50 +0800

    2021

--- src/views/Notice.vue
+++ src/views/Notice.vue
@@ -8,12 +8,12 @@
 		</div>
 		<div class="notice notice2">
 			<jc-notice>
-				公告栏 notice 以封装功能为主，样式尽可能让用户自行编写
+				公告栏组件以封装功能为主，用户可以自定义公告栏的样式
 			</jc-notice>
 		</div>
 		<div class="notice">
 			<jc-notice :icon="false">
-				joce-ui 为个人组件库，主要是因为网络上未发现有相似的组件，或者不能满足需求进而开发的一套 vue 组件库，所以“实用” 和 “唯一” 是此组件库的特点。
+				jctool 为个人组件库，主要是因为网络上未发现有相似的组件，或者不能满足需求进而开发的一套 vue 组件库，所以“实用” 和 “唯一” 是此组件库的特点。
 			</jc-notice>
 		</div>
 		<titler>如何使用</titler>
